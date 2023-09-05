7584b3bdb6777be4b3a982b26e0fa8649451d261
En Rong <53928333+chownces@users.noreply.github.com>
2023-09-05 14:46:06 +0800

    chore(backend-api): add API versioning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();

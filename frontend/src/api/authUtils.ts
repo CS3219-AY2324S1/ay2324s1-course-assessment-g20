23402520b3c83c2e0f5cc4c993814e97eaef921c
Aiken Wong <aikenwong55@gmail.com>
2023-09-19 18:02:50 +0800

    Fix multiple refresh token requests

--- frontend/src/api/authUtils.ts
+++ frontend/src/api/authUtils.ts
@@ -1,15 +1,24 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import { IAuthContext } from '../interfaces';
+import { IAuth, IAuthContext } from '../interfaces';
 import { backendServicesPaths, BACKEND_API_HOST, VERSION_PREFIX } from '../utils/constants';
+
+let refreshingFunc: undefined | Promise<IRefreshTokenResponse> = undefined;
 
 interface RetryConfig extends AxiosRequestConfig {
   retry?: boolean;
 }
 
-const refreshAccessToken = (refreshToken: string) =>
-  axios.post(`${BACKEND_API_HOST}${VERSION_PREFIX}${backendServicesPaths.auth.refresh}`, {
-    refreshToken: refreshToken,
-  });
+interface IRefreshTokenResponse {
+  data: IAuth;
+  status: number;
+}
+
+const refreshAccessToken = async (refreshToken: string) =>
+  await axios
+    .post(`${BACKEND_API_HOST}${VERSION_PREFIX}${backendServicesPaths.auth.refresh}`, {
+      refreshToken: refreshToken,
+    })
+    .then((resp) => resp as IRefreshTokenResponse);
 
 // The auth interceptor is used to intercept requests to the backend and add the access token to the request.
 // In the event that the access token is expired, the interceptor will attempt to refresh the access token.
@@ -45,31 +54,44 @@ export const generateAuthInterceptor = (authContext: IAuthContext) => {
     async function (error) {
       const originalRequest = error.config;
 
-      // unauthenticated, try to refresh tokens
-      if (error.response.status === 401 && !originalRequest.retry) {
-        originalRequest.retry = true;
-        const refreshToken = authContext.authState?.refreshToken;
-        let authState = authContext.authState;
-        if (refreshToken) {
-          try {
-            const resp = await refreshAccessToken(refreshToken);
-            if (resp.status === 201) {
-              authState = resp.data;
-            }
-          } catch (e) {
-            // refreshing tokens failed, sign out user
-            authContext.signout();
-            return Promise.reject(e);
-          }
-          originalRequest.headers.Authorization = `Bearer ${authState?.accessToken}`;
-          authContext.signIn(authState!);
-          return protectedRequests(originalRequest);
-        }
-      } else {
+      if (error.response.status !== 401) {
+        // not an auth error, return error
+        return Promise.reject(error);
+      }
+
+      if (originalRequest.retry === true) {
+        // we have already retried once, sign out user
+        authContext.signout();
+        return Promise.reject(error);
+      }
+
+      originalRequest.retry = true;
+      const refreshToken = authContext.authState?.refreshToken;
+      let authState = authContext.authState;
+
+      if (!refreshToken) {
         // user has no refresh token, sign out user
         authContext.signout();
         return Promise.reject(error);
       }
+
+      try {
+        if (!refreshingFunc) {
+          refreshingFunc = refreshAccessToken(refreshToken);
+        }
+        const resp = await refreshingFunc;
+        if (resp.status === 201) {
+          authState = resp.data;
+        }
+      } catch (e) {
+        // refreshing tokens failed, sign out user
+        authContext.signout();
+        return Promise.reject(e);
+      }
+      originalRequest.headers.Authorization = `Bearer ${authState?.accessToken}`;
+      authContext.signIn(authState!);
+
+      return await protectedRequests(originalRequest);
     },
   );
   return { protectedRequests, protectedRequestConfig };

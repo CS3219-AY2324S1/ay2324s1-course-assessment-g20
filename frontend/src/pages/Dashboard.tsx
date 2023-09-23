ba80442f26668ff1a3b72ba543e7403e7a264ede
TohWeiJie01 <tohweijie01@gmail.com>
2023-09-24 01:00:19 +0800

    Add dashboard (question bank section of main menu)

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,58 +1,111 @@
 import {
   Box,
+  Button,
   Paper,
+  styled,
   Table,
   TableBody,
   TableCell,
+  tableCellClasses,
   TableContainer,
   TableHead,
   TableRow,
   Typography,
 } from '@mui/material';
+import { useState } from 'react';
+import Popup from '../components/Popup';
 
 // TODO: Replace this with a real table
 function createData(
   id: number,
   title: string,
-  description: string,
   category: string,
   complexity: string,
+  description: string,
 ) {
-  return { id, title, description, category, complexity };
+  return { id, title, category, complexity, description };
 }
 
 const rows = [
-  createData(1, 'Reverse a String', 'Description 1', 'Strings, Algorithms', 'Easy'),
-  createData(2, 'Repeated DNA Sequences', 'Description 2', 'Data Structures, Algorithms', 'Medium'),
-  createData(3, 'Sliding Window Maximum', 'Description 3', 'Arrays, Algorithms', 'Hard'),
+  createData(1, 'Reverse a String', 'Strings, Algorithms', 'Easy', 'Description 1'),
+  createData(2, 'Repeated DNA Sequences', 'Data Structures, Algorithms', 'Medium', 'Description 2'),
+  createData(3, 'Sliding Window Maximum', 'Arrays, Algorithms', 'Hard', 'Description 3'),
 ];
 
 export default function Dashboard() {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+  const [rowIndex, setRowIndex] = useState(-1);
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  const [popupVisibility, setPopupVisibility] = useState(false);
+  const handleOnClick = (num: number) => {
+    setRowIndex(num);
+    setPopupVisibility(true);
+  };
+  const handleOnClose = () => setPopupVisibility(false);
+
   return (
     <Box>
-      <Typography variant="h2">PeerPrep Dashboard</Typography>
+      <Typography variant="h2" color="green" align="center">
+        Questions
+      </Typography>
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell align="left">Title</TableCell>
-              <TableCell align="left">Category&nbsp;(g)</TableCell>
-              <TableCell align="left">Complexity&nbsp;(g)</TableCell>
-              {/* <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
+              <StyledTableCell align="left">No.</StyledTableCell>
+              <StyledTableCell align="left">Title</StyledTableCell>
+              <StyledTableCell align="left">Category</StyledTableCell>
+              <StyledTableCell align="left">Complexity</StyledTableCell>
+              <StyledTableCell align="left">Description</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
-              <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">
+              <StyledTableRow
+                key={row.id}
+                onClick={() => {
+                  handleSelectItem(row.title);
+                }}
+              >
+                <StyledTableCell component="th" scope="row">
                   {row.id}
-                </TableCell>
-                <TableCell align="left">{row.title}</TableCell>
-                <TableCell align="left">{row.category}</TableCell>
-                <TableCell align="left">{row.complexity}</TableCell>
-                {/* <TableCell align="right">{row.protein}</TableCell> */}
-              </TableRow>
+                </StyledTableCell>
+                <StyledTableCell align="left">{row.title}</StyledTableCell>
+                <StyledTableCell align="left">{row.category}</StyledTableCell>
+                <StyledTableCell align="left">{row.complexity}</StyledTableCell>
+                <StyledTableCell align="left">
+                  {popupVisibility && rowIndex == row.id && (
+                    <Popup
+                      title={row.title}
+                      children={row.description}
+                      openPopup={true}
+                      setOpenPopup={handleOnClose}
+                    ></Popup>
+                  )}
+                  <Button onClick={() => handleOnClick(row.id)}>READ</Button>
+                </StyledTableCell>
+              </StyledTableRow>
             ))}
           </TableBody>
         </Table>

19387069f831b1f7c88e7e94bc07b5c6a4294d16
TohWeiJie01 <tohweijie01@gmail.com>
2023-09-24 01:01:59 +0800

    Add MainMenu page (with matching component and question bank component)

---
+++ frontend/src/pages/MainMenu.tsx
@@ -0,0 +1,45 @@
+import { Button, Stack, Typography } from '@mui/material';
+import Dashboard from './Dashboard';
+
+export default function MainMenu() {
+  return (
+    <>
+      <Typography variant="h2" align="center" color="black">
+        MATCH WITH SOMEONE
+      </Typography>
+      <Typography align="center" component={'span'}>
+        <Stack display={'block'} spacing={2} direction={'row'}>
+          <Button
+            variant={'contained'}
+            color={'success'}
+            size={'large'}
+            onClick={() => console.log('CLICKED EASY')}
+            aria-label="Large sizes"
+          >
+            EASY
+          </Button>
+          <Button
+            variant={'contained'}
+            color={'warning'}
+            size={'large'}
+            onClick={() => console.log('CLICKED MEDIUM')}
+            aria-label="Large sizes"
+          >
+            MEDIUM
+          </Button>
+          <Button
+            variant={'contained'}
+            color={'error'}
+            size={'large'}
+            onClick={() => console.log('CLICKED HARD')}
+            aria-label="Large sizes"
+          >
+            HARD
+          </Button>
+        </Stack>
+      </Typography>
+      <h1></h1>
+      <Dashboard></Dashboard>
+    </>
+  );
+}

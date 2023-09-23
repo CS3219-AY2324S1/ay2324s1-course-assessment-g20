9c6f2782c2e1430458c8c847b4f58703061e0684
TohWeiJie01 <tohweijie01@gmail.com>
2023-09-24 00:58:10 +0800

    Add popup component

---
+++ frontend/src/components/Popup.tsx
@@ -0,0 +1,30 @@
+import { 
+  Button, 
+  Dialog, 
+  DialogContent, 
+  DialogTitle 
+} from "@mui/material";
+
+interface PopupProps {
+  title: string;
+  children: string;
+  openPopup: boolean;
+  setOpenPopup: () => void;
+}
+
+export default function Popup({ title, children, openPopup, setOpenPopup }: PopupProps) {
+
+  return (
+    <Dialog open={openPopup}>
+      <DialogTitle style={{justifyContent: "space-between"}}>
+        {title}
+        <Button type="button" className="btn-close" onClick={setOpenPopup} aria-label="Close">
+          X
+        </Button>
+      </DialogTitle>
+      <DialogContent>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}

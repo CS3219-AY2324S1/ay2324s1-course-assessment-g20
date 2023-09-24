b77e03f2c7dec748a8e2f8e6ac7e61a2c5745d2c
TohWeiJie01 <tohweijie01@gmail.com>
2023-09-24 17:40:06 +0800

    Change styling

--- frontend/src/components/Popup.tsx
+++ frontend/src/components/Popup.tsx
@@ -3,7 +3,7 @@ import {
   Dialog, 
   DialogContent, 
   DialogTitle 
-} from "@mui/material";
+} from '@mui/material';
 
 interface PopupProps {
   title: string;
@@ -13,18 +13,26 @@ interface PopupProps {
 }
 
 export default function Popup({ title, children, openPopup, setOpenPopup }: PopupProps) {
-
   return (
     <Dialog open={openPopup}>
-      <DialogTitle style={{justifyContent: "space-between"}}>
+      <DialogTitle style={{ justifyContent: 'space-between' }}>
         {title}
-        <Button type="button" className="btn-close" onClick={setOpenPopup} aria-label="Close">
+        <Button 
+          type="button" 
+          className="btn-close" 
+          onClick={setOpenPopup} 
+          aria-label="Close"
+          style={{ fontSize: '20px', padding: "0px 0px" }}
+          sx={{
+            width: 10,
+            height: 25,
+            backgroundColor: 'white',
+          }}
+        >
           X
         </Button>
       </DialogTitle>
-      <DialogContent>
-        {children}
-      </DialogContent>
+      <DialogContent>{children}</DialogContent>
     </Dialog>
   );
 }

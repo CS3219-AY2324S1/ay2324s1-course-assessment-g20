fb53a6d0b2aa49dd71f7f23c4f83f62146a44b5f
En Rong <53928333+chownces@users.noreply.github.com>
2023-09-27 23:31:32 +0800

    refactor(backend): add constants for interservice communication

--- backend/apps/gateway/src/oauthProviders/google/google-oauth.strategy.ts
+++ backend/apps/gateway/src/oauthProviders/google/google-oauth.strategy.ts
@@ -6,11 +6,12 @@ import { ConfigService } from '@nestjs/config';
 import { AuthProvider } from '@app/types/authProvider';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
+import { AUTH_SERVICE, AuthServiceApi } from '@app/interservice-api/auth';
 
 @Injectable()
 export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(
-    @Inject('AUTH_SERVICE') private readonly authServiceClient: ClientProxy,
+    @Inject(AUTH_SERVICE) private readonly authServiceClient: ClientProxy,
     configService: ConfigService,
   ) {
     const googleOauthOptions: _StrategyOptionsBase =
@@ -34,7 +35,7 @@ export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
     const { id, name, emails } = profile;
 
     const user = await firstValueFrom(
-      this.authServiceClient.send('find_or_create_oauth_user', {
+      this.authServiceClient.send(AuthServiceApi.FIND_OR_CREATE_OAUTH_USER, {
         authProvider: AuthProvider.GOOGLE,
         authProviderId: id,
         email: emails[0].value,

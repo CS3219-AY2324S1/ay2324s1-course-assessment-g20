fee48fa476e53648e59d3474ac2f63aa8bc2d098
TohWeiJie01 <tohweijie01@gmail.com>
2023-10-02 02:28:45 +0800

    Abstract out and implement Matching Service frontend

---
+++ frontend/src/pages/Matching.tsx
@@ -0,0 +1,80 @@
+import { Box, Button, Stack, Typography } from '@mui/material';
+import WaitingScreen from '../components/WaitingScreen';
+import { useState } from 'react';
+
+export default function Matching() {
+  // Usestate to handle the difficulty selected
+  const [difficultyLevel, setDifficultyLevel] = useState('');
+
+  // Usestate and functions to handle the loading screen visibility
+  const [searchingVisibility, setSearchingVisibility] = useState(false);
+  const handleSearchingOnClick = (diff: string) => {
+    setDifficultyLevel(diff);
+    setSearchingVisibility(true);
+  };
+  const handlePopupOnClose = () => setSearchingVisibility(false);
+
+  return (
+    <Box>
+      <Typography
+        variant="h2"
+        align="center"
+        color="white"
+        sx={{
+          backgroundColor: 'grey',
+        }}
+      >
+        MATCH WITH SOMEONE
+      </Typography>
+      <br />
+      <Typography align="center" component={'span'}>
+        <Stack display={'block'} spacing={2} direction={'row'}>
+          {searchingVisibility && (
+            <WaitingScreen
+              title="SEARCHING FOR PARTNER - "
+              difficulty={difficultyLevel}
+              openPopup={true}
+              setOpenPopup={handlePopupOnClose}
+            ></WaitingScreen>
+          )}
+          <Button
+            variant={'contained'}
+            onClick={() => handleSearchingOnClick('EASY')}
+            style={{ fontSize: '50px' }}
+            sx={{
+              width: 170,
+              height: 75,
+              backgroundColor: 'green',
+            }}
+          >
+            EASY
+          </Button>
+          <Button
+            variant={'contained'}
+            onClick={() => handleSearchingOnClick('MEDIUM')}
+            style={{ fontSize: '50px' }}
+            sx={{
+              width: 230,
+              height: 75,
+              backgroundColor: 'orange',
+            }}
+          >
+            MEDIUM
+          </Button>
+          <Button
+            variant={'contained'}
+            onClick={() => handleSearchingOnClick('HARD')}
+            style={{ fontSize: '50px' }}
+            sx={{
+              width: 170,
+              height: 75,
+              backgroundColor: 'red',
+            }}
+          >
+            HARD
+          </Button>
+        </Stack>
+      </Typography>
+    </Box>
+  );
+}

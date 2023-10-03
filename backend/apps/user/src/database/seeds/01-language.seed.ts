94ad78353c474184beddaa6bf5671ccbfd5ac485
Bryan Kwok <bryankwokyf@gmail.com>
2023-10-03 10:41:28 +0800

    refactor(backend): add enums for languages and roles

--- backend/apps/user/src/database/seeds/01-language.seed.ts
+++ backend/apps/user/src/database/seeds/01-language.seed.ts
@@ -1,12 +1,22 @@
 import { Knex } from 'knex';
 
+export enum Language {
+  JAVASCRIPT = 1,
+  TYPESCRIPT = 2,
+}
+
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries and reset primary key
   await knex.raw('TRUNCATE TABLE languages RESTART IDENTITY CASCADE');
 
   // Inserts seed entries
-  await knex('languages').insert([
-    { id: 1, name: 'JavaScript' },
-    { id: 2, name: 'TypeScript' },
-  ]);
+  await knex('languages').insert(
+    Object.entries(Language)
+      .filter(
+        ([key, value]) => typeof key === 'string' && typeof value === 'number',
+      )
+      .map(([key, value]) => {
+        return { id: value, name: key };
+      }),
+  );
 }
--- backend/apps/user/src/database/seeds/02-role.seed.ts
+++ backend/apps/user/src/database/seeds/02-role.seed.ts
@@ -1,12 +1,22 @@
 import { Knex } from 'knex';
 
+export enum Role {
+  MAINTAINER = 1,
+  REGULAR = 2,
+}
+
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries and reset primary key
   await knex.raw('TRUNCATE TABLE roles RESTART IDENTITY CASCADE');
 
   // Inserts seed entries
-  await knex('roles').insert([
-    { id: 1, name: 'maintainer' },
-    { id: 2, name: 'regular' },
-  ]);
+  await knex('roles').insert(
+    Object.entries(Role)
+      .filter(
+        ([key, value]) => typeof key === 'string' && typeof value === 'number',
+      )
+      .map(([key, value]) => {
+        return { id: value, name: key };
+      }),
+  );
 }

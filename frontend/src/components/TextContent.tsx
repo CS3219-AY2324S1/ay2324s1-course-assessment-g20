a0cd34a5e4a4fc1733c2c304e98f2275fe24b824
BryannYeap <e0543723@u.nus.edu>
2023-10-16 02:19:06 +0800

    feat: add tables to markdown

--- frontend/src/components/TextContent.tsx
+++ frontend/src/components/TextContent.tsx
@@ -1,7 +1,8 @@
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 function TextContent({ content }: { content: string }) {
-  return <ReactMarkdown>{content}</ReactMarkdown>;
+  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
 }
 
 export default TextContent;

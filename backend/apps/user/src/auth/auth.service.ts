7771d4c4c7d641a99039370cfdca1b90e5d61a83
Bryan Kwok <bryankwokyf@gmail.com>
2023-10-19 10:33:49 +0800

    feat(auth-roles): shift role checking from jwt to user service database

--- backend/apps/user/src/auth/auth.service.ts
+++ backend/apps/user/src/auth/auth.service.ts
@@ -51,10 +51,7 @@ export class AuthService {
    * If undefined, create a new database entry (i.e. when signing in)
    */
   async generateJwts(user, oldToken?: string) {
-    const payload: JwtPayload = {
-      id: user.id,
-      roleId: user.userProfile.roleId,
-    };
+    const payload: JwtPayload = { id: user.id };
     const accessToken: string = this.generateAccessToken(payload);
     const refreshToken: string = this.generateRefreshToken(payload);
 

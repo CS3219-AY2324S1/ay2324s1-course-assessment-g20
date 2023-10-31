68c4df423407c925a99353c077a2973046c8277d
Bryan Kwok <bryankwokyf@gmail.com>
2023-10-31 16:22:26 +0800

    backend: create nestjs library for mocks

--- backend/apps/user/src/database/seeds/04-userProfile.seed.ts
+++ backend/apps/user/src/database/seeds/04-userProfile.seed.ts
@@ -1,10 +1,6 @@
+import 'tsconfig-paths/register';
 import { Knex } from 'knex';
-import {
-  MOCK_USER_1_UUID,
-  MOCK_USER_2_UUID,
-  MOCK_USER_1_NAME,
-  MOCK_USER_2_NAME,
-} from './03-user.seed';
+import { MOCK_ADMIN_USER_PROFILE, MOCK_USER_1_PROFILE } from '@app/mocks';
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEPLOYMENT = ['staging', 'production'].includes(NODE_ENV);
@@ -20,19 +16,7 @@ export async function seed(knex: Knex): Promise<void> {
 
   // Inserts seed entries
   await knex('user_profiles').insert([
-    {
-      userId: MOCK_USER_1_UUID,
-      name: MOCK_USER_1_NAME,
-      preferredLanguageId: 1,
-      roleId: 1,
-      username: 'alicetan',
-    },
-    {
-      userId: MOCK_USER_2_UUID,
-      name: MOCK_USER_2_NAME,
-      preferredLanguageId: 2,
-      roleId: 2,
-      username: 'boblim',
-    },
+    MOCK_ADMIN_USER_PROFILE,
+    MOCK_USER_1_PROFILE,
   ]);
 }
---
+++ backend/libs/mocks/src/user.ts
@@ -0,0 +1,56 @@
+import { User, UserProfile } from "@app/microservice/interfaces/user";
+import { JwtPayload } from "@app/types";
+import { Language } from "@app/types/languages";
+import { Role } from "@app/types/roles";
+
+export const MOCK_ADMIN_USER_UUID = '4394cce2-7f04-41f2-8ade-8b21cad1cb20';
+export const MOCK_ADMIN_USER_NAME = 'Alice Tan';
+export const MOCK_ADMIN_USER_EMAIL = 'alicetan@gmail.com';
+export const MOCK_ADMIN_USER_OAUTH_ID = 'random-oauth-id-1';
+
+export const MOCK_USER_1_UUID = '030eeafc-26cc-4e16-8467-f55b818689fa';
+export const MOCK_USER_1_NAME = 'Bob Lim';
+export const MOCK_USER_1_EMAIL = 'boblim@gmail.com';
+export const MOCK_USER_1_OAUTH_ID = 'random-oauth-id-2';
+
+export const MOCK_AUTH_PROVIDER = 'test_provider';
+
+export const MOCK_ADMIN_USER_PROFILE: UserProfile = {
+  userId: MOCK_ADMIN_USER_UUID,
+  name: MOCK_ADMIN_USER_NAME,
+  preferredLanguageId: Language.JAVASCRIPT,
+  roleId: Role.MAINTAINER,
+};
+
+export const MOCK_USER_1_PROFILE: UserProfile = {
+  userId: MOCK_USER_1_UUID,
+  name: MOCK_USER_1_NAME,
+  preferredLanguageId: Language.TYPESCRIPT,
+  roleId: Role.REGULAR,
+};
+
+export const MOCK_ADMIN_USER: User = {
+  id: MOCK_ADMIN_USER_UUID,
+  authProvider: MOCK_AUTH_PROVIDER,
+  authProviderId: MOCK_ADMIN_USER_OAUTH_ID,
+  oauthName: MOCK_ADMIN_USER_NAME,
+  email: MOCK_ADMIN_USER_EMAIL,
+  userProfile: MOCK_ADMIN_USER_PROFILE,
+};
+
+export const MOCK_USER_1: User = {
+  id: MOCK_USER_1_UUID,
+  authProvider: MOCK_AUTH_PROVIDER,
+  authProviderId: MOCK_USER_1_OAUTH_ID,
+  oauthName: MOCK_USER_1_NAME,
+  email: MOCK_USER_1_EMAIL,
+  userProfile: MOCK_USER_1_PROFILE,
+};
+
+export const MOCK_ADMIN_USER_JWT_PAYLOAD: JwtPayload = {
+  id: MOCK_ADMIN_USER_UUID,
+};
+
+export const MOCK_USER_1_JWT_PAYLOAD: JwtPayload = {
+  id: MOCK_USER_1_UUID,
+};

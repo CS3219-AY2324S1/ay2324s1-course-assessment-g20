2f0651a174b52727395a978cf5578ae2c930ee4d
Bryan Kwok <bryankwokyf@gmail.com>
2023-10-31 16:29:58 +0800

    backend: add usernames to mock users

--- backend/libs/mocks/src/user.ts
+++ backend/libs/mocks/src/user.ts
@@ -4,12 +4,14 @@ import { Language } from "@app/types/languages";
 import { Role } from "@app/types/roles";
 
 export const MOCK_ADMIN_USER_UUID = '4394cce2-7f04-41f2-8ade-8b21cad1cb20';
-export const MOCK_ADMIN_USER_NAME = 'Alice Tan';
+export const MOCK_ADMIN_NAME = 'Alice Tan';
+export const MOCK_ADMIN_USERNAME = 'alicetan';
 export const MOCK_ADMIN_USER_EMAIL = 'alicetan@gmail.com';
 export const MOCK_ADMIN_USER_OAUTH_ID = 'random-oauth-id-1';
 
 export const MOCK_USER_1_UUID = '030eeafc-26cc-4e16-8467-f55b818689fa';
 export const MOCK_USER_1_NAME = 'Bob Lim';
+export const MOCK_USER_1_USERNAME = 'boblim';
 export const MOCK_USER_1_EMAIL = 'boblim@gmail.com';
 export const MOCK_USER_1_OAUTH_ID = 'random-oauth-id-2';
 
@@ -17,7 +19,8 @@ export const MOCK_AUTH_PROVIDER = 'test_provider';
 
 export const MOCK_ADMIN_USER_PROFILE: UserProfile = {
   userId: MOCK_ADMIN_USER_UUID,
-  name: MOCK_ADMIN_USER_NAME,
+  name: MOCK_ADMIN_NAME,
+  username: MOCK_ADMIN_USERNAME,
   preferredLanguageId: Language.JAVASCRIPT,
   roleId: Role.MAINTAINER,
 };
@@ -25,6 +28,7 @@ export const MOCK_ADMIN_USER_PROFILE: UserProfile = {
 export const MOCK_USER_1_PROFILE: UserProfile = {
   userId: MOCK_USER_1_UUID,
   name: MOCK_USER_1_NAME,
+  username: MOCK_USER_1_USERNAME,
   preferredLanguageId: Language.TYPESCRIPT,
   roleId: Role.REGULAR,
 };
@@ -33,7 +37,7 @@ export const MOCK_ADMIN_USER: User = {
   id: MOCK_ADMIN_USER_UUID,
   authProvider: MOCK_AUTH_PROVIDER,
   authProviderId: MOCK_ADMIN_USER_OAUTH_ID,
-  oauthName: MOCK_ADMIN_USER_NAME,
+  oauthName: MOCK_ADMIN_NAME,
   email: MOCK_ADMIN_USER_EMAIL,
   userProfile: MOCK_ADMIN_USER_PROFILE,
 };
